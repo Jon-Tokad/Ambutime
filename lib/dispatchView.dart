f96868fbeb4640d4fbe0e3c8d8bb31db8bc37c4d
Jason Bodzy <jasonbodzy@gmail.com>
2024-01-20 17:20:57 -0500

    dispact updates

--- lib/dispatchView.dart
+++ lib/dispatchView.dart
@@ -1,12 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:ambutime/db/ambulanceDb.dart';
 import 'package:ambutime/db/emergencyRequestDb.dart';
 import 'ambulanceView.dart';
 
-Future<void> ambulanceConnect() async {
+Future<List<Map<String, dynamic>>?> ambulanceConnect() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ambulancesDatabase.connect();
-  await emergencyRequestsDb.connect();
+  return await ambulancesDatabase.pullAmbulances();
 }
 
 class DispatchRoute extends StatelessWidget {
@@ -15,25 +17,114 @@ class DispatchRoute extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ambulanceConnect();
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.red,
         ),
         home: Scaffold(
-          body: Center(
+          body: AmbulanceList(),
+          /*Center(
             child: Padding(
               padding: const EdgeInsets.all(25.0),
               child: Column(children: [
                 TitleBar(),
                 Row(
-                  children: [],
+                  children: [AmbulanceList()],
                 )
               ]),
             ),
-          ),
+          ),*/
         ));
+  }
+}
+
+class Ambulance extends StatelessWidget {
+  const Ambulance(
+      {super.key,
+      required this.id,
+      required this.available,
+      required this.location});
+  final String id;
+  final String available;
+  final String location;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ContentText(text: id),
+        ContentText(text: available),
+        ContentText(text: location)
+      ],
+    );
+  }
+}
+
+class AmbulanceList extends StatefulWidget {
+  const AmbulanceList({super.key});
+
+  @override
+  State<AmbulanceList> createState() => _AmbulanceListState();
+}
+
+class _AmbulanceListState extends State<AmbulanceList> {
+  var ambulanceList = [];
+
+  void _getAmbulances() async {
+    await ambulancesDatabase.connect();
+    var ambulanceListtmp = await ambulancesDatabase.pullAmbulances();
+
+    var objList = [];
+
+    for (var item in ambulanceListtmp!) {
+      objList.add({
+        "id": item['_id'].toString(),
+        "available": item['available'].toString(),
+        "location": item['location'].toString(),
+      });
+    }
+
+    setState(() {
+      ambulanceList = objList;
+      print(ambulanceList);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: _getAmbulances,
+            tooltip: 'Increment',
+            child: const Icon(Icons.refresh),
+          ),
+          Text("Ambulances"),
+          ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: ambulanceList.length,
+              itemBuilder: (BuildContext context, int index) {
+                var curID = ambulanceList[index]["id"].toString();
+                var curAv = ambulanceList[index]["available"].toString();
+                var curLoc = ambulanceList[index]["location"].toString();
+                return ListTile(
+                  title: Text(
+                    "ID: $curID",
+                    style: TextStyle(fontSize: 10),
+                  ),
+                  subtitle: Text("Availability: $curAv",
+                      style: TextStyle(fontSize: 10)),
+                  trailing: Text(
+                    "Location: $curLoc",
+                    style: TextStyle(fontSize: 10),
+                  ),
+                );
+              })
+        ],
+      ),
+    );
   }
 }
 
