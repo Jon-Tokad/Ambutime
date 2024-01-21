e2862de217ae284b8cdbb8a6eec15ae9189432a8
Jon Tokad <jtokad@purdue.edu>
2024-01-20 21:08:01 -0500

    Update mapView.dart

--- lib/mapView.dart
+++ lib/mapView.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+
 
 class MapRoute extends StatelessWidget {
   const MapRoute({super.key});
@@ -28,44 +32,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class CupertinoButtonExample extends StatelessWidget {
-  const CupertinoButtonExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('CupertinoButton Sample'),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const CupertinoButton(
-              onPressed: null,
-              child: Text('Disabled'),
-            ),
-            const SizedBox(height: 30),
-            const CupertinoButton.filled(
-              onPressed: null,
-              child: Text('Disabled'),
-            ),
-            const SizedBox(height: 30),
-            CupertinoButton(
-              onPressed: () {},
-              child: const Text('Enabled'),
-            ),
-            const SizedBox(height: 30),
-            CupertinoButton.filled(
-              onPressed: () {},
-              child: const Text('Enabled'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 class TitleBar extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -96,19 +62,67 @@ class TitleBar extends StatelessWidget {
   }
 }
 
+class LocationPin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.red,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Icon(
+        Icons.location_on,
+        color: Colors.white,
+        size: 28,
+      ),
+    );
+  }
+}
+
+// for reference (not used)
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Location location = Location();
+  LocationData? currentLocation;
+  TextEditingController latitudeController = TextEditingController();
+  TextEditingController longitudeController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    try {
+      LocationData locationData = await location.getLocation();
+      setState(() {
+        currentLocation = locationData;
+        latitudeController.text = currentLocation?.latitude.toString() ?? '';
+        longitudeController.text = currentLocation?.longitude.toString() ?? '';
+      });
+    } catch (e) {
+      print("Error getting location: $e");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+        
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -126,6 +140,39 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'ETA: $_counter minutes',
             ),
+            Expanded(
+              child: 
+                FlutterMap(
+                  options: MapOptions(
+                  initialCenter: LatLng(currentLocation!.latitude ?? 0, currentLocation!.longitude ?? 0),
+                  initialZoom: 18.0,
+
+                ),
+                children: [
+                  TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                  ),
+                  MarkerLayer(
+  markers: [
+    Marker(
+      point: LatLng(currentLocation!.latitude ?? 0, currentLocation!.longitude ?? 0),
+      width: 30,
+      height: 30,
+      child: LocationPin(),
+    ),
+  ],
+),
+                  RichAttributionWidget(
+                  attributions: [
+                    TextSourceAttribution(
+                      'OpenStreetMap contributors',
+                ),
+            ],
+          ),
+        ],
+    )
+            ,)
           ],
         ),
       ),
